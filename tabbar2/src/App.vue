86139d46dfff2323c9fd679d25f0b20cbf8fbdd7
“enguang123” <2310553384@qq.com>
2022-08-19 21:58:30 +0800

    vue tabbar

---
+++ tabbar2/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <MainTabBar></MainTabBar>
+  </div>
+</template>
+
+<script>
+import MainTabBar from '@/components/tabbar/MainTabBar.vue'
+export default {
+  name: 'App',
+  components: {
+    MainTabBar
+  }
+}
+</script>
+<style>
+
+</style>
